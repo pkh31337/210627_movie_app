2604561e165ded523819389b8d03b56a4eff7d42
unknown <khpark1810@gmail.com>
2021-06-27 18:51:52 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-function Food({ fav }) {
-  console.log({ fav });
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src = {picture} />
+    </div>;
 }
+
+const foodIlike = [
+  {
+    name: "Kimchi",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FCUvEAsTUcAANUnd.jpg&f=1&nofb=1"
+  },
+  {
+    name: "Samgiopsal",
+    image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.blaud.co.kr%2Fuploads%2Ffw1604005%2F201909%2F4419185ba11a7cc2b1d5f54363f2b0eb.jpg&f=1&nofb=1"
+  },
+  {
+    name: "Bibimbap",
+    image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ffoodmoa.co.kr%2Fdata%2Ffile%2Ffood%2F1401%2Ftvmz_BRT0046117_1_Ka73AVA5f5UAc.jpg&f=1&nofb=1"
+  },
+  {
+    name: "Kimbap",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FeeUNnlFVV7o%2Fmaxresdefault.jpg&f=1&nofb=1"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodIlike.map(dish => (
+        <Food name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
