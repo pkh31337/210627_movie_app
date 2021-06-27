6659fc4849c14acf9f97772cef744874f19cb459
unknown <khpark1810@gmail.com>
2021-06-27 18:28:27 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Potato from "./Potato";
+function Food({ fav }) {
+  console.log({ fav });
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
