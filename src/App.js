cb92eb00c06ee1f1bbe9157d2f76f89c90a6a5e4
unknown <khpark1810@gmail.com>
2021-06-27 22:48:17 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
-import PropTypes from "prop-types";
+import React from "react";
+import axios from "axios";
 
-const foodIlike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FCUvEAsTUcAANUnd.jpg&f=1&nofb=1",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgiopsal",
-    image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.blaud.co.kr%2Fuploads%2Ffw1604005%2F201909%2F4419185ba11a7cc2b1d5f54363f2b0eb.jpg&f=1&nofb=1",
-    rating: 4.8
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ffoodmoa.co.kr%2Fdata%2Ffile%2Ffood%2F1401%2Ftvmz_BRT0046117_1_Ka73AVA5f5UAc.jpg&f=1&nofb=1",
-    rating: 3.2
-  },
-  {
-    id: 4,
-    name: "Kimbap",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FeeUNnlFVV7o%2Fmaxresdefault.jpg&f=1&nofb=1",
-    rating: 4.7
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const movies = await axios.get("http://yts-proxy.now.sh/list_movies.json");
   }
-];
-
-function Food({ name, picture, rating }) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src = {picture} alt={name} />
-    <h4>{rating} / 5.0</h4>
-    </div>;
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div className="App">
-      {foodIlike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-      ))}
-    </div>
-  );
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div> {isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
